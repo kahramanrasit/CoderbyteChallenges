473ec9841e057e6a7fbda85447cbe3cdce70912e
kahramanrasit <34141599+kahramanrasit@users.noreply.github.com>
2021-07-23 17:47:44 +0300

    Create SimpleMode.py

---
+++ SimpleMode.py
@@ -0,0 +1,24 @@
+def SimpleMode(arr):
+  
+  l = []
+  for e in arr:
+    if e not in l:
+      l.append(e)
+  
+  cnt = [0 for _ in l]
+  
+
+  for e in arr:
+    cnt[l.index(e)] += 1
+
+  
+  if max(cnt) > 1:
+    return l[cnt.index(max(cnt))]
+
+  
+  return -1
+
+  
+
+# keep this function call here 
+print(SimpleMode(input()))
