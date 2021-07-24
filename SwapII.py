fae99693da9213bb9832a8af5d0cd94e8f123255
kahramanrasit <34141599+kahramanrasit@users.noreply.github.com>
2021-07-24 11:21:19 +0300

    Update SwapII.py

--- SwapII.py
+++ SwapII.py
@@ -1,34 +1,36 @@
 def SwapII(strParam):
-#"123gg))((" the output was incorrect. The correct output is 123GG))((
-  s = strParam.split()
-
-  for i in range(len(s)):
-    if s[i].isalpha():
-      s[i] = s[i].swapcase()
-      continue
-    idx = 0
-    l = list(s[i])
-    flag = False
-    for k in range(len(l)):
-      if l[k].isdigit() and flag == False and k != len(l) - 1 and not l[k + 1].isdigit():
-        idx = k
-        flag = True
-      elif l[k].isdigit() and flag == True:
-        t = l[idx]
-        l[idx] = l[k]
-        l[k] = t
-      
-
-      if not l[k].isalnum() and flag == True:
-        flag = False
-        idx = None
-
-    s[i] = ''.join(l)
-    s[i] = s[i].swapcase()
-
-  strParam = ' '.join(s)
  
-  return strParam
+  strParam = strParam.swapcase()
+  word = strParam.split()
+  res = []
+  
+  for e in word:
+    l = list(e)
+    idx = 0
+    flag = False
+    cnt = [i for i in l if i.isdigit()]
+    if len(cnt) >= 3:
+      res.append(''.join(l))
+      continue
+    else:
+      for i in range(len(l)):
+        if l[i].isdigit() and flag == False:
+          idx = i
+          flag = True
+        elif not l[i].isalnum() and flag == True:
+          flag = False
+        elif l[i].isdigit() and flag == True:
+          t = l[idx]
+          l[idx] = l[i]
+          l[i] = t
 
+    res.append(''.join(l))
+    
+
+  strParam = ' '.join(res)
+  
+  return strParam
+      
+      
 # keep this function call here 
 print(SwapII(input()))
